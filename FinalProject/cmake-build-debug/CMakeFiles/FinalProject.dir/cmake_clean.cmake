bc7b47234e63c75f07c5da3d198a4ce8fb008d4c
cmartano <34069445+cmartano@users.noreply.github.com>
2017-11-28 11:42:04 -0500

    Add files via upload

---
+++ FinalProject/cmake-build-debug/CMakeFiles/FinalProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FinalProject.dir/main.cpp.o"
+  "FinalProject.pdb"
+  "FinalProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FinalProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
