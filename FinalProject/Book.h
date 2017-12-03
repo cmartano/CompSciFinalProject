0e9b5a31625c8532c43b94e6a939f518c0530f67
keselh54 <hkesel@ithaca.edu>
2017-12-03 13:43:56 -0500

    trying to find our external libraries... we aren't able to use the string type without external libraries

--- FinalProject/Book.h
+++ FinalProject/Book.h
@@ -5,5 +5,18 @@
 #ifndef COMPSCIFINALPROJECT_BOOK_H
 #define COMPSCIFINALPROJECT_BOOK_H
 
-#endif //COMPSCIFINALPROJECT_BOOK_H
 
+
+
+class Book{
+private:
+    std::string title;
+
+
+public:
+
+};
+
+
+
+#endif //COMPSCIFINALPROJECT_BOOK_H
