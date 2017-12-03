4e770652926b2c9bc2ab20b2e54fec2d3f9fac9d
keselh54 <hkesel@ithaca.edu>
2017-12-03 09:42:05 -0500

    I added what we need to do by tomorrow.

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -5,4 +5,5 @@ int main() {
     return 0;
     //hey
     //yo
+    //Today: We need to write up the basic backbone of the project. That includes the main.cpp, the store.h, store.cpp, inventory.h, and book.h.
 }
