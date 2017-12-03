a3e6194b188c27d5fce97d92e3f2a2c213e8e993
keselh54 <hkesel@ithaca.edu>
2017-12-03 13:51:33 -0500

    trying to find our external libraries... we aren't able to use the string type without external libraries

--- FinalProject/Book.h
+++ FinalProject/Book.h
@@ -5,12 +5,17 @@
 #ifndef COMPSCIFINALPROJECT_BOOK_H
 #define COMPSCIFINALPROJECT_BOOK_H
 
-
+#include "LinkedNode.h"
 
 
 class Book{
 private:
     std::string title;
+    int wantValue;
+    int haveValue;
+    LinkedNode* waitingList;
+
+
 
 
 public:
