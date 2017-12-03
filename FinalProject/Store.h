a0d3652a7a03f83f24e52b07482dd084b2813241
keselh54 <hkesel@ithaca.edu>
2017-12-03 14:37:53 -0500

    started the general store stuff too

--- FinalProject/Store.h
+++ FinalProject/Store.h
@@ -5,4 +5,17 @@
 #ifndef COMPSCIFINALPROJECT_STORE_H
 #define COMPSCIFINALPROJECT_STORE_H
 
+
+#include "LinkedNode.h"
+
+class Store{
+
+private:
+    LinkedNode* inventory;
+    int titlesCount;
+    std::string* arrayOfTitles;
+
+public:
+};
+
 #endif //COMPSCIFINALPROJECT_STORE_H
