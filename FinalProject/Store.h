23c05557dc8296935e867feaf98f11ef8d7b230a
keselh54 <hkesel@ithaca.edu>
2017-12-04 10:59:38 -0500

    i added //hey

--- FinalProject/Store.h
+++ FinalProject/Store.h
@@ -67,6 +67,44 @@ public:
      */
     void sell(std::string title);
 
+    /**
+     * Create a bulk purchase order for additional books based on a comparison of the have and want values
+     * in the inventory. For each book, enough books should be ordered so that, when the order is received,
+     * the have value will be equal to the want value.
+     * @param title
+     */
+    void order(std::string title);
+
+    /**
+     * Take information from a file listing the delivery shipment of books.
+     * The file will contain the title and the count of each book included in the shipment.
+     * Read the file, and update the have values in the inventory accordingly (the employee
+     * will then shelve the actual books from the shipment). Note that the program must add
+     * any item to the inventory if the delivered title is not present in the current inventory
+     * (for instance if an extra book not requested was delivered).
+     * If there is a wait list for any books delivered, the needed copies of that book
+     * should not be added to the inventory or be put on shelves. Instead, the program
+     * should print the title of the book and the information of the person it should
+     * be put on hold for. The employee then calls, texts, or  emails (depending on customer preference)
+     * to inform the person that their book is waiting, writes a note marking who is picking up the book,
+     * and leaves the book by the counter. The book is then considered sold
+     * (the person should be removed from wait list).
+     */
+    void delivery();
+
+    /**
+     * Write a return invoice to a file specifying all books to be returned.
+     * For each book, copies should be returned if the have value is higher
+     * than the want value. The system should change the have value to equal
+     * the want value, and the output file should then give the list of books
+     * to take off the shelf and return.
+     */
+    void returnInvoice();
+
+    /**
+     * Save the inventory and wait lists in a file and terminate execution.
+     */
+    void quit();
 
 };
 
