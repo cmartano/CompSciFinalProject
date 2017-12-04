0311ad0194e8c48c1b843406339c38fa76de577e
cmartano <clmartano@gmail.com>
2017-12-04 11:20:14 -0500

    test

---
+++ FinalProject/CustomerWaiting.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Christian Martano on 12/3/17.
+//
+
+#include <string>
+#include "CustomerWaiting.h"
+
+std::string getContactPref(){
+    return customerWaiting.contactPref;
+}
+
+std::string getEmail(){
+    return customerWaiting.email;
+}
+
+std::string getPhoneNumber(){
+    return customerWaiting.phoneNumber;
+}
+
+std::string getName(){
+    return customerWaiting.name;
+}
+
+
+
+
+
