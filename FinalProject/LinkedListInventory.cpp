f22e2deaad1125297902ac36719545c9dc374d95
cmartano <clmartano@gmail.com>
2017-12-12 21:15:07 -0500

    working on linked list inventory. also need to add some setters and getters into book

--- FinalProject/LinkedListInventory.cpp
+++ FinalProject/LinkedListInventory.cpp
@@ -7,11 +7,48 @@
 
 
 void LinkedListInventory:: add(std::string title, int haveValue, int wantValue) {
-    if (first!= nullptr){
-        first=new LinkedNode(new Book(title,haveValue,wantValue));
+    if (first != nullptr) {
+        first = new LinkedNode(new Book(title, haveValue, wantValue));
+        end = first;
     }
 
+    else {
+            LinkedNode *newNode = new LinkedNode(new Book(title, haveValue, wantValue));
+            LinkedNode *temp = end;
+            temp->setNext(newNode);
+            end = newNode;
+        }
+    }
+
+void LinkedListInventory::modify(std::string title) {
+    while (first != nullptr){
+
+        if (first->getItem().getTitle==title){
+            std::cout<<"Enter a new want value:"<<std::endl;
+            std::cin>>wantval;
+            first->getItem().setWantValue(wantval);
+        }
+
+        else{
+            first=first->getNext();
+        }
+    }
 }
+
+void LinkedListInventory::sell(std::string title) {
+    while (first != nullptr){
+
+        if (first->getItem().getTitle==title){
+            first->getItem().setHaveValue(first->getItem().getHaveValue-1);
+        }
+
+        else{
+            first=first->getNext();
+        }
+    }
+}
+
+
 
 
 
