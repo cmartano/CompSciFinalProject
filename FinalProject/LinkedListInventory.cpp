e46cb4c04d7e7d0cfd22af30e99e28d493025933
keselh54 <hkesel@ithaca.edu>
2017-12-14 21:25:32 -0500

    Changed stuff regarding adding a book to the list

--- FinalProject/LinkedListInventory.cpp
+++ FinalProject/LinkedListInventory.cpp
@@ -30,7 +30,7 @@ void LinkedListInventory:: add() {
 
     Book book1= Book(title, haveValue, wantValue);
     LinkedNode* newNode = new LinkedNode(book1);
-    if (first != nullptr) {
+    if (first == nullptr) {
         first = newNode;
         last = first;
     }
